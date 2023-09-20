ac1ccb3d1ad55cea547d4375466fca64c4cb26e8
christina8711 <christina.nguyen8711@gmail.com>
2023-09-20 11:01:12 -0400

    CubeStackingAssistant API Key

---
+++ CubeStackingAssistant.py
@@ -0,0 +1,7 @@
+import openai  # Import the OpenAI library
+
+
+class CubeStackingAssistant:
+    # Import OpenAI API Key
+    def __init__(self, api_key='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'):
+        self.api_key = api_key
